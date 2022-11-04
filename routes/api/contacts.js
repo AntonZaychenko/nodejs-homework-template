b3655e4718d32024e0e8de722bb7366710a1df4a
AntonZaychenko <105227367+AntonZaychenko@users.noreply.github.com>
2022-11-04 12:50:14 +0200

    qw

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,46 @@
-const express = require('express')
+// const express = require('express')
 
-const router = express.Router()
+// const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.get('/', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.get('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.post('/', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.delete('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.put('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-module.exports = router
+// module.exports = router
+
+const express = require("express");
+const { validation, ctrlWrapper } = require("../../middlewares");
+const { contactsSchema, patchSchema } = require("../../schemas");
+const { contacts: ctrl } = require("../../controller");
+
+const router = express.Router();
+
+router.get("/", ctrlWrapper(ctrl.getAll));
+
+router.get("/:contactId", ctrlWrapper(ctrl.getById));
+
+router.post("/", validation(contactsSchema), ctrlWrapper(ctrl.add));
+
+router.delete("/:contactId", ctrlWrapper(ctrl.remove));
+
+router.put("/:contactId", validation(contactsSchema), ctrlWrapper(ctrl.update));
+
+router.patch("/:contactId", validation(patchSchema), ctrlWrapper(ctrl.patch));
+
+module.exports = router;
